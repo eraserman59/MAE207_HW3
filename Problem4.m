88bf55264d208847194b8c341abb4666b2d68259
eraserman59 <55302930+eraserman59@users.noreply.github.com>
2023-11-26 13:49:08 -0800

    Update Problem4.m
    updated

--- Problem4.m
+++ Problem4.m
@@ -1,4 +1,5 @@
 clear
+close all
 Mu = 1;
 rInitial = [1,0,0];
 vInitial = [0,1,0];
@@ -21,12 +22,15 @@ plot(a)
 title('semiMajorAxis vs Time graph')
 subplot(2,2,2)
 plot(e)
+ylim([0,2])
 title('eccentricity vs Time graph')
 subplot(2,2,3)
 plot(i) 
+ylim([0,60])
 title('inclination vs Time Graph')
 subplot(2,2,4)
 plot(M)
+ylim([0,60])
 title('True Anomoly vs Time Graph')
 saveas(gcf,'Oribtal1.jpg')
 
@@ -50,12 +54,15 @@ plot(a)
 title('ad = 0.01 semiMajorAxis vs Time graph')
 subplot(2,2,2)
 plot(e)
+ylim([0,2])
 title('ad = 0.01 eccentricity vs Time graph')
 subplot(2,2,3)
 plot(i) 
+ylim([0,60])
 title('ad = 0.01 inclination vs Time Graph')
 subplot(2,2,4)
 plot(M)
+ylim([0,60])
 title('ad = 0.01 True Anomoly vs Time Graph')
 saveas(gcf,'Oribtal2.jpg')
 
@@ -79,12 +86,15 @@ plot(a)
 title('ad = 0.1 semiMajorAxis vs Time graph')
 subplot(2,2,2)
 plot(e)
+ylim([0,2])
 title('ad = 0.1 eccentricity vs Time graph')
 subplot(2,2,3)
 plot(i) 
+ylim([0,60])
 title('ad = 0.1 inclination vs Time Graph')
 subplot(2,2,4)
 plot(M)
+ylim([0,60])
 title('ad = 0.1 True Anomoly vs Time Graph')
 saveas(gcf,'Oribtal3.jpg')
 
@@ -98,22 +108,25 @@ figure
 plot(X,Y); 
 title('ad = 1 Position graph')
 saveas(gcf,'Position4.jpg')
-a= koeVector(:,1);
-e = koeVector(:,2);
-i = koeVector(:,3);
-M = koeVector(:,6);
+a= koeVector(:,1)
+e = koeVector(:,2)
+i = koeVector(:,3)
+M = koeVector(:,6)
 figure
 subplot(2,2,1)
 plot(a)
 title('ad = 1 semiMajorAxis vs Time graph')
 subplot(2,2,2)
 plot(e)
+ylim([0,2])
 title('ad = 1 eccentricity vs Time graph')
 subplot(2,2,3)
 plot(i) 
+ylim([0,60])
 title('ad = 1 inclination vs Time Graph')
 subplot(2,2,4)
 plot(M)
+ylim([0,60])
 title('ad = 1 True Anomoly vs Time Graph')
 saveas(gcf,'Oribtal4.jpg')
 
