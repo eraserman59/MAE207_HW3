4b64c68d61bd8a305438a79b944ca191d4d5ba75
eraserman59 <55302930+eraserman59@users.noreply.github.com>
2023-12-08 16:43:03 -0800

    Delete PosUpdate.m

--- PosUpdate.m
+++
@@ -1,9 +0,0 @@
-function [ PosFinal, VelFinal] = PosUpdate(Mu, Pos, Vel, Distrubance)
-%Updates the position, velocity and accel, and orbital elements of the
-%system. 
-
-Vel1 = VelUpdate(Mu, Pos, Vel, Distrubance);
-PosFinal = Pos + Vel1;
-VelFinal = VelUpdate(Mu, PosFinal, Vel1, Distrubance);
-
-end
